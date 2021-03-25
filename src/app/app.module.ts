cd44b4bb9906ce8d12306ff9b7e6cad9eab0ebe0
liviulenter <lenterliviu@gmail.com>
2021-03-25 14:35:59 +0100

    add src files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import {RouterModule} from '@angular/router';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import { FooterComponent } from './footer/footer.component';
+import { HouseListComponent } from './house-list/house-list.component';
+import {MatCardModule} from '@angular/material/card';
+import { HouseNewComponent } from './house-new/house-new.component';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
+import { HouseViewComponent } from './house-view/house-view.component';
+import { HouseSearchComponent } from './house-search/house-search.component';
+import {MatListModule} from '@angular/material/list';
+import { HouseEditComponent } from './house-edit/house-edit.component';
+import { AddressViewComponent } from './address-view/address-view.component';
+import {HttpClientModule} from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    FooterComponent,
+    HouseListComponent,
+    HouseNewComponent,
+    HouseViewComponent,
+    HouseSearchComponent,
+    HouseEditComponent,
+    AddressViewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    FormsModule,
+    MatListModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
